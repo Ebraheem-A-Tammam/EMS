415c6ebf9e777039f5f854164c45cddef5545568
ebraheem <ebraheem20012020@gmail.com>
2023-02-28 00:38:14 +0200

    remiving Win init till find the desired commands

--- init/init.cmd
+++
@@ -1,8 +0,0 @@
-echo "Setting up environment for first time..."
-
-echo "Creating database"
-sqlite3 system.db < db/tables.sql
-
-echo "Setting up environment finished"
-echo "_______________________________"
-echo ""
--- run.cmd
+++ run.cmd
@@ -1,13 +1,4 @@
-if [ -e system.db ]
-then
-    echo "running the program..."
-    echo "______________________"
-    echo ""
-    python main.py
-else
-    ./init/init.cmd
-    echo "running the program..."
-    echo "______________________"
-    echo ""
-    python main.py
-fi
+echo "running the program..."
+echo "______________________"
+echo ""
+python main.py
