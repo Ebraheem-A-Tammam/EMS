2e25aa6b40bab79611eab10dd7902a8bf8613899
ebraheem <ebraheem20012020@gmail.com>
2023-02-28 19:10:55 +0200

    more convenient name

---
+++ __init__/__init__.sh
@@ -0,0 +1,24 @@
+echo "Setting up environment for first time..."
+
+REQUIRED_PKG="sqlite3"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+echo Checking for $REQUIRED_PKG: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+  sudo apt-get --yes install $REQUIRED_PKG
+fi
+
+REQUIRED_PKG="python3"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+echo Checking for $REQUIRED_PKG: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+  sudo apt-get --yes install $REQUIRED_PKG
+fi
+
+echo "Creating database"
+sqlite3 system.db < db/tables.sql
+
+echo "Setting up environment finished"
+echo "_______________________________"
+echo "" 
